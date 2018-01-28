04ce554c9d4ef497a9c52cf69d1f4087600b1ae1
Justyna Wasiak <jw224@wp.pl>
2018-01-28 11:03:10 +0100

    zadanie 4, 5, 6

--- zadanie_5/zadanie_5.rb
+++ zadanie_5/zadanie_5.rb
@@ -3,8 +3,9 @@ NAME = 'username'
 PASSWORD = 'password'
 LOGIN = 'login'
 
-class Zadanie3
-  driver = Selenium::WebDriver.for :chrome
+class Zadanie5
+  caps = Selenium::WebDriver::Remote::Capabilities.new()
+  driver = Selenium::WebDriver.for(:firefox, :desired_capabilities => caps)
   driver.navigate.to('http://localhost/litecart/admin/')
   driver.find_element(:name, NAME).send_keys('admin')
   driver.find_element(:name, PASSWORD).send_keys('admin')
--- zadanie_6/zadanie_6.rb
+++ zadanie_6/zadanie_6.rb
@@ -3,8 +3,9 @@ NAME = 'username'
 PASSWORD = 'password'
 LOGIN = 'login'
 
-class Zadanie3
-  driver = Selenium::WebDriver.for :chrome
+class Zadanie6
+  Selenium::WebDriver::Firefox::Binary.path = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
+  driver = Selenium::WebDriver.for(:firefox)
   driver.navigate.to('http://localhost/litecart/admin/')
   driver.find_element(:name, NAME).send_keys('admin')
   driver.find_element(:name, PASSWORD).send_keys('admin')
